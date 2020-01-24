f1e0460f055f44cc211b6d9738f248733f2288e4
Hazmi35 <contacthzmi@gmail.com>
2020-01-24 20:58:10 +0700

    hmm

---
+++ src/Routes/finalizeLogin.js
@@ -0,0 +1,12 @@
+const { get } = require("node-superfetch");
+
+const run = async (client, request, response) => {
+  const res = await get(
+    `https://api.zealcord.xyz/getCookie?code=${response.query.code}`
+  );
+  console.log(response.query.code);
+  const body = await res.body;
+};
+
+const meta = { path: "/finalizeLogin", name: "Finalize Login", method: "GET" };
+module.exports = { run, meta };
