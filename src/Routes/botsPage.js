3a0e9cfbfa08ea0f9d47e15d9aabd312ca561938
HZMI - Hazmi35 <contacthzmi@gmail.com>
2020-01-24 21:19:16 +0700

    Update botsPage.js

--- src/Routes/botsPage.js
+++ src/Routes/botsPage.js
@@ -1,12 +1,12 @@
 const run = async (client, request, response) => {
   console.log(request.params);
-    response.status(200).render("botlist.ejs", {
-      bot: client,
-      path: request.path,
-      req: request,
-      res: response,
-      zealcord: await client.zealcord
-    });
+  response.status(200).render("botlist.ejs", {
+    bot: client,
+    path: request.path,
+    req: request,
+    res: response,
+    zealcord: await client.zealcord
+  });
 };
 
 const meta = { path: "/bots/:page", name: "Bot List", method: "GET" };
