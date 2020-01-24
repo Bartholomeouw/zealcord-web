107ab6ff6e9d76e459dacc5f62c76742aa4c1ed9
Hazmi35 <contacthzmi@gmail.com>
2020-01-24 21:17:45 +0700

    banyak yang konflifk, dwii lu ngapain sih anjeng?

--- src/Routes/botsPage.js
+++ src/Routes/botsPage.js
@@ -1,18 +1,12 @@
 const run = async (client, request, response) => {
   console.log(request.params);
-  if (client.maintenance.get("botlist") === true)
-    return require("./defaults/404");
-  if (!request.params.page) {
-    response.redirect("/bots/1");
-  } else {
-    response.status(200).render("botlist.ejs", {
-      bot: client,
-      path: request.path,
-      req: request,
-      res: response,
-      zealcord: await client.zealcord
-    });
-  }
+  response.status(200).render("botlist.ejs", {
+    bot: client,
+    path: request.path,
+    req: request,
+    res: response,
+    zealcord: await client.zealcord
+  });
 };
 
 const meta = { path: "/bots/:page", name: "Bot List", method: "GET" };
