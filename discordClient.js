86ba6a47ad68dcc12eceb8bb773175dc67abfd67
Zealcord <xchangenation@gmail.com>
2020-03-01 04:56:41 +0100

    Apani?

--- discordClient.js
+++ discordClient.js
@@ -4,7 +4,7 @@ module.exports = class WebClient extends Client {
   constructor(...args) {
     super(...args);
 
-    this.owners = ["304377187057008645"];
-    this.version = "WebClient v0.0.1 by DwiiUnknown#3704";
+    this.owners = ["319872685897416725", "390045370240991234", "304377187057008645", "290159952784392202"];
+    this.version = "WebClient v0.0.1 by Zealcord Development Team";
   }
 }
