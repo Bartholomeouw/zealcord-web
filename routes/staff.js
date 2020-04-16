a99fc2c6df3276a7ffcb9a9bb64c926e02c2bf52
Satya Bagus Dwiatmaja <dwii5359@azalelnation.com>
2020-04-16 19:59:34 +0700

    Update staff.js

--- routes/staff.js
+++ routes/staff.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const NewClient = require('../discordClient.js');
+const client = new NewClient({
+  fetchAllMember: true,
+  disableEveryone: false
+});
+
+client.on("ready", () => {
+  console.log(`Client Loaded.. ${client.user.id}`);
+})
+
+client.login(process.env.SECRET);
+
 router.get('/staff', async function (req, res, next) {
 
   res.render('stafflist', {
     title: 'Zealcord Nation',
+    bot: client,
     req: req,
     res: res,
   });
